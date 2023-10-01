f90b48fc15bda08ae1c4d1b496839998f43d4156
alemoles <alemolerio@gmail.com>
2023-10-01 20:43:32 -0300

    define board and board size

--- src/app/pages/board-page/board-page.component.ts
+++ src/app/pages/board-page/board-page.component.ts
@@ -1,5 +1,42 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
+const BLOCK_SIZE: number = 20;
+const BOARD_WIDTH: number = 14;
+const BOARD_HEIGHT: number = 30;
+
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+]
+
 @Component({
   selector: 'app-board-page',
   templateUrl: './board-page.component.html',
@@ -11,24 +48,41 @@ export class BoardPageComponent implements AfterViewInit {
   private context?: CanvasRenderingContext2D;
 
   constructor() {
-    this.canvas = {} as ElementRef<HTMLCanvasElement>;    
+    this.canvas = {} as ElementRef<HTMLCanvasElement>;
   }
 
   ngAfterViewInit(): void {
     const context = this.canvas.nativeElement.getContext('2d');
     if (context !== null) {
-        this.context = context;
-        this.draw();
+      this.context = context;
+      this.canvas.nativeElement.width = BLOCK_SIZE * BOARD_WIDTH;
+      this.canvas.nativeElement.height = BLOCK_SIZE * BOARD_HEIGHT;
+      this.context.scale(BLOCK_SIZE, BLOCK_SIZE);
+      requestAnimationFrame(() => this.update());
     } else {
-        console.error('2D context not supported');
+      console.error('2D context not supported');
     }
+  }
+
+  //2. game loop
+  update(): void {
+    this.draw();
+    requestAnimationFrame(() => this.update());
   }
 
   draw(): void {
     if (this.context) {
-      // Your drawing code here
-      this.context.fillStyle = 'red';
-      this.context.fillRect(0, 0, 200, 200);
+      this.context.fillStyle = '#000';
+      this.context.fillRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+      board.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value === 1) {
+            this.context!.fillStyle = 'yellow'
+            this.context!.fillRect(x, y, 1, 1)
+          }
+        })
+      })
     }
   }
+
 }
