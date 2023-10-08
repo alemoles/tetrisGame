c5b34f04f5f396eb5a029a5594b33f2ab4ceacfb
alemoles <alemolerio@gmail.com>
2023-10-08 11:40:55 -0300

    add first start page style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { BoardPageComponent } from './pages/board-page/board-page.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 const routes: Routes = [
   {
@@ -9,13 +10,17 @@ const routes: Routes = [
     component: LayoutPageComponent,
     children: [
       {
+        path: 'start-game',
+        component: StartPageComponent,
+      },
+      {
         path: 'board',
         component: BoardPageComponent,
 
       },
       {
         path: '**',
-        redirectTo: 'board'
+        redirectTo: 'start-game'
       }
     ]
   }
