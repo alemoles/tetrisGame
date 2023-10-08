8dd52f6b2a120aeb5a590ca5c0d458d63f88b153
alemoles <alemolerio@gmail.com>
2023-10-08 10:49:21 -0300

    add start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { BoardPageComponent } from './pages/board-page/board-page.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutPageComponent,
-    BoardPageComponent
+    BoardPageComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
