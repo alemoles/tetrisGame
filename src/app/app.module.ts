41bd8c60b399be93d857af270f03a4bcbd72e661
alemoles <alemolerio@gmail.com>
2023-10-13 20:45:41 -0300

    add score component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { BoardPageComponent } from './pages/board-page/board-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ScorePageComponent } from './pages/score-page/score-page.component';
 import { StartPageComponent } from './pages/start-page/start-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutPageComponent,
     BoardPageComponent,
-    StartPageComponent
+    LayoutPageComponent,
+    ScorePageComponent,
+    StartPageComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/score-page/score-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-page',
+  templateUrl: './score-page.component.html',
+  styleUrls: ['./score-page.component.css']
+})
+export class ScorePageComponent {
+
+}
