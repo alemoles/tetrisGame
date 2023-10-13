da5a36f1d9c820c3bd5ea3fbcafcf834c73c1d29
alemoles <alemolerio@gmail.com>
2023-10-13 20:46:39 -0300

    add score page component to app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+
 import { BoardPageComponent } from './pages/board-page/board-page.component';
-import { StartPageComponent } from './pages/start-page/start-page.component';
 import { CanDeactivateGuard } from './guards/can-deactivate.guard';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ScorePageComponent } from './pages/score-page/score-page.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 const routes: Routes = [
   {
@@ -18,7 +20,11 @@ const routes: Routes = [
         path: 'board',
         component: BoardPageComponent,
         canDeactivate: [CanDeactivateGuard]
-
+        
+      },
+      {
+        path: 'scores',
+        component: ScorePageComponent,
       },
       {
         path: '**',
