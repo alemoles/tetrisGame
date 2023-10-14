7b9d47db73f22bcc9d1f35e51fb49ccb350c25c0
alemoles <alemolerio@gmail.com>
2023-10-13 21:11:45 -0300

    add getScores and add scores

--- src/app/services/scores.service.ts
+++ src/app/services/scores.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
+import { Score } from '../interfaces/score.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ScoresService {
     private baseUrl: string = environments.baseUrl;
     constructor(private httpClient: HttpClient) { }
 
+    getScores(): Observable<Score[]> {
+        return this.httpClient.get<Score[]>(`${this.baseUrl}/scores`);
+    }
+
+    addScore(score: Score): Observable<Score> {
+        return this.httpClient.post<Score>(`${this.baseUrl}/scores`, score);
+    }
 }
