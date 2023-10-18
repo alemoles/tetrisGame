6dc9b6334f95e8d2cce76434a7000ee43215db0a
alemoles <alemolerio@gmail.com>
2023-10-17 22:33:50 -0300

    sort loaded elements from BE an get only  first tenth

--- src/app/services/scores.service.ts
+++ src/app/services/scores.service.ts
@@ -1,19 +1,45 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from 'src/environments/environments';
 import { Score } from '../interfaces/score.interface';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class ScoresService {
+export class ScoresService implements OnInit {
+
     private baseUrl: string = environments.baseUrl;
+    private _scores: Score[] = [];
+
     constructor(private httpClient: HttpClient) { }
 
-    getScores(): Observable<Score[]> {
+    ngOnInit(): void {
+        this.loadInitialScores();
+    }
+
+    private loadInitialScores(): void {
+        this.getScores().subscribe(scores => {
+            this._scores = scores.sort((a, b) => b.score - a.score).slice(0, 10);
+        });
+    }
+
+    public getScores(): Observable<Score[]> {
         return this.httpClient.get<Score[]>(`${this.baseUrl}/scores`);
     }
 
-    addScore(score: Score): Observable<Score> {
-        return this.httpClient.post<Score>(`${this.baseUrl}/scores`, score);
+    public addScore(score: number, name: string): Observable<Score> {
+
+        const newScore = {
+            "username": name,
+            "score": score
+        }
+        return this.httpClient.post<Score>(`${this.baseUrl}/scores`, newScore);
+    }
+
+    get scores(): Score[] {
+        return [...this._scores];
+    }
+
+    get lowestScore(): number {
+        return this._scores.length ? this._scores[this._scores.length - 1].score : 0;
     }
 }
