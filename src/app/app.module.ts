c366536313ba1e6442112d30787dfb6e52c3552c
alemoles <alemolerio@gmail.com>
2023-10-17 22:53:12 -0300

    inject HttpClientModule dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -19,7 +20,8 @@ import { StartPageComponent } from './pages/start-page/start-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/score-page/score-page.component.ts
+++ src/app/pages/score-page/score-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Score } from 'src/app/interfaces/score.interface';
+import { ScoresService } from 'src/app/services/scores.service';
 
 @Component({
   selector: 'app-score-page',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./score-page.component.css']
 })
 export class ScorePageComponent {
+  private _scores: Score[] = [];
+
+  constructor(
+    private scoresService: ScoresService
+  ) {
+    this.updateScores()
+  }
+
+  public updateScores() {
+    this._scores = this.scoresService.scores;
+  }
+
+  get scores(): Score[] {
+    return this._scores;
+  }
 
 }
